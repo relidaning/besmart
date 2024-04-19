e890ee5a8b9b1f7850253ea1cd9a2d1c22238774
lidaning <41282656+relidaning@users.noreply.github.com>
2024-04-20 01:44:19 +0800

    language reactor controller app

---
+++ languagereactorcontroller/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, render_template
+import os
+from dotenv import load_dotenv
+from pynput.keyboard import Key, Controller
+
+load_dotenv()
+PORT = os.getenv('PORT')
+DEBUG = True if os.getenv('DEBUG') == 'True' else False
+app = Flask(__name__)
+keyboard = Controller()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/option')
+def option():
+    option = request.args.get('o', '')
+    print(f'[#] Request /option, and option is: {option}')
+    if option!='':
+        keyboard.press(option)
+        keyboard.release(option)
+    return {'code': 200, 'data': 'Button  clicked...'}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=PORT, debug=DEBUG)
