5e6682e9f5a227030dca346bd16f0cbd118d0dc5
lidaning <41282656+relidaning@users.noreply.github.com>
2025-01-09 01:56:26 +0800

    auth app update

--- auth/src/app.py
+++ auth/src/app.py
@@ -1,14 +1,12 @@
-from flask import Flask, request, render_template
-from pyauthtools import jwtauthtool
+from flask import Flask, request, render_template, redirect
+from pyauthtools.jwtauthtool import auth, encode
 from flask_sqlalchemy import SQLAlchemy
 import os
 from dotenv import load_dotenv
 
-
 load_dotenv()
 DEBUG = True if os.getenv('DEBUG') == 'True' else False
 SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
-JWT_SECRET = os.getenv('JWT_SECRET')
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 db = SQLAlchemy(app)
@@ -18,22 +16,12 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255))
     password = db.Column(db.String(255))
-    
+
 
 @app.route('/')
+@auth
 def index():
-  try:
-    token = request.headers.get('Authorization').split(' ')[1]
-    result = jwtauthtool.decode_jwt(token, JWT_SECRET)
-    if result == 'Unauthorized':
-      result = 'Unauthorized', 401
-      return render_template('login.html', result=result)
-    else:
-      result = 'success', 200
-      return render_template('index.html', result)
-  except:
-    result = 'Unauthorized', 401
-    return render_template('login.html', result=result)
+  return render_template('index.html')
 
 
 @app.route('/login', methods=['POST'])
@@ -45,23 +33,11 @@ def login():
     result = 'Unauthorized', 401
     return render_template('login.html', result=result)
   else:
-    result = jwtauthtool.create_jwt(username, JWT_SECRET), 200
+    result = encode({'username': username}), 200
+    print(f'login success!, token: {result}')
     return render_template('index.html', result=result)
   
   
-@app.route('/validate', methods=['POST', 'GET']) 
-def validate():
-  try:
-    token = request.headers.get('Authorization').split(' ')[1]
-    result = jwtauthtool.decode_jwt(token, JWT_SECRET)
-    if result == 'Unauthorized':
-      return 'Unauthorized', 401
-    else:
-      return result, 200
-  except:
-    return 'Unauthorized', 401
-  
-
 PORT = os.getenv('PORT')
 NACOS_SERVER_URL = os.getenv('NACOS_SERVER_URL')
 SERVICE_NAME = os.getenv('SERVICE_NAME')
