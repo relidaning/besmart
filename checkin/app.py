8ab3470ee1586d909430d00447af7c81a45a6712
lidaning <41282656+relidaning@users.noreply.github.com>
2026-01-04 01:26:35 +0800

    restructure db file.

--- checkin/app.py
+++ checkin/app.py
@@ -1,7 +1,5 @@
-from dataclasses import dataclass
 from flask import Flask, render_template, request, jsonify
 from datetime import datetime, timedelta
-from flask_sqlalchemy import SQLAlchemy
 from apscheduler.schedulers.background import BackgroundScheduler
 from sqlalchemy import desc
 
@@ -16,34 +14,7 @@ load_dotenv(env_file)
 DEBUG = True if os.getenv('DEBUG') == 'True' else False
 SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
-db = SQLAlchemy(app)
 scheduler = BackgroundScheduler()
-
-
-class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    task_id = db.Column(db.Integer)
-    task_name = db.Column(db.String(119))
-    task_date = db.Column(db.Date())
-    is_completed = db.Column(db.String(1))
-    complete_time = db.Column(db.Date())
-    is_timeout = db.Column(db.String(1))
-    schedule_type = db.Column(db.String(1))
-
-
-class Schedule(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    schedule_name = db.Column(db.String(120))
-    schedule_type = db.Column(db.String(2))
-    score = db.Column(db.Float)
-    is_valid = db.Column(db.String(2))
-
-
-@dataclass
-class Score(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    score_date: str = db.Column(db.Date())
-    score: float = db.Column(db.Float)
 
 
 @app.route('/hello_checkin')
---
+++ checkin/db.py
@@ -0,0 +1,36 @@
+#
+# This is a db module, and data class
+# 
+from dataclasses import dataclass
+from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from app import app
+
+
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer)
+    task_name = db.Column(db.String(119))
+    task_date = db.Column(db.Date())
+    is_completed = db.Column(db.String(1))
+    complete_time = db.Column(db.Date())
+    is_timeout = db.Column(db.String(1))
+    schedule_type = db.Column(db.String(1))
+
+class Schedule(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    schedule_name = db.Column(db.String(120))
+    schedule_type = db.Column(db.String(2))
+    score = db.Column(db.Float)
+    is_valid = db.Column(db.String(2))
+
+@dataclass
+class Score(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    score_date: str = db.Column(db.Date())
+    score: float = db.Column(db.Float)
+
+
+
